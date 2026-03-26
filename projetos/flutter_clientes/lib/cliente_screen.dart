f989506af8ec07fbdc4bd3b5cf3a93ba716af757
erickin <grisosteerick25@gmail.com>
2026-03-25 22:23:43 -0300

    primeiro commit

---
+++ projetos/flutter_clientes/lib/cliente_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class ClienteScreen extends StatefulWidget {
+  const ClienteScreen({super.key});
+
+  @override
+  State<ClienteScreen> createState() => _ClienteScreenState();
+}
+
+class _ClienteScreenState extends State<ClienteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Cadastro de Clientes")),
+        backgroundColor: Colors.blueAccent,
+        foregroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          TextField(decoration: InputDecoration(labelText: "Nome")),
+          TextField(decoration: InputDecoration(labelText: "Sobrenome")),
+          TextField(decoration: InputDecoration(labelText: "Idade")),
+          SizedBox(
+            width: double.infinity,
+            child: FloatingActionButton(
+              child: Text("Carregar"),
+              onPressed: () {}),
+          ),
+          Text("Nome: Alex, Sobrenome: da Arrocha, idade: 29")
+        ],
+      ),
+    );
+  }
+}
